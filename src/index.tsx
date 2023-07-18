f7bd3b132a5b6ce684954439f2aa3aa9df4e3df3
erprilianoAbbas <erprilianosaputra.abbas@medirecords.com>
2023-07-18 18:33:16 +0700

    Fix: Router

--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { ApolloProvider } from "@apollo/client";
 import { client } from "./api";
+import { HashRouter } from "react-router-dom";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 
@@ -11,7 +12,9 @@ const root = ReactDOM.createRoot(
 root.render(
   <React.StrictMode>
     <ApolloProvider client={client}>
-      <App />
+      <HashRouter>
+        <App />
+      </HashRouter>
     </ApolloProvider>
   </React.StrictMode>,
 );
